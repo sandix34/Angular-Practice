a07158e5218bd64c69367793c41c0ae682853907
sandrine <sandrinezocca@gmail.com>
2021-01-16 16:47:11 +0100

    testing app

--- angular-routing/src/app/app.component.spec.ts
+++ angular-routing/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [RouterTestingModule]
     }).compileComponents();
   });
 
@@ -26,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-routing app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('angular-routing');
   });
 });
